b9417b61e4eab651b8cfeb99061d4105d9b4a035
victor chi <victoriamglorious@gmail.com>
2025-11-15 20:06:56 +0100

    removed the text properties from the slideData objects

--- slideData.js
+++ slideData.js
@@ -1,31 +1,23 @@
 export const slideData = [
     {
-        backgroundColour: 'khaki',
-        backgroundColour: 'blue',
-        text: `This is the first slide with ${this.backgroundColour} background.`
+        backgroundColour: 'khaki'
     },
     {
-        backgroundColour: 'orange',
-        text: `This is the second slide with ${this.backgroundColour} background.`
+        backgroundColour: 'orange'
     },
     {
-        backgroundColour: 'blue',
-        text: `This is the third slide with ${this.backgroundColour} background.`
+        backgroundColour: 'blue'
     },
     {
-        backgroundColour: 'lightsalmon',
-        text: `This is the fourth slide with ${this.backgroundColour} background.`
+        backgroundColour: 'lightsalmon'
     },
     {
-        backgroundColour: 'red',
-        text: `This is the fifth slide with ${this.backgroundColour} background.`
+        backgroundColour: 'red'
     },
     {
-        backgroundColour: 'green',
-        text: `This is the sixth slide with ${this.backgroundColour} background.`
+        backgroundColour: 'green'
     },
     {
-        backgroundColour: 'magenta',
-        text: `This is the seventh slide with ${this.backgroundColour} background.`
+        backgroundColour: 'magenta'
     },
 ]
